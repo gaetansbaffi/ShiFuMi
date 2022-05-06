90c1a736c7b86e7075b36b701d923ecf66693e84
gaetansbaffi <gaetansbaffi@live.fr>
2022-05-06 15:09:50 +0200

    Basic game is functionning Added score, vilain score, result

--- app.js
+++ app.js
@@ -3,28 +3,34 @@ let rock, paper, scissors;
 rock = "rock";
 paper = "paper";
 scissors = "scissors";
+let vScore = 0;
+let pScore = 0;
 
+//random choice
 function computerPLay() {
   let options = [rock, paper, scissors];
   let random = Math.floor(Math.random() * 3);
-
   return options[random];
 }
 
+//single round logic
 function playRound(playerSelection, computerSelection = computerPLay()) {
-  console.log(`player = ${playerSelection}, computer = ${computerSelection}`);
   if (playerSelection === computerSelection) {
-    return console.log("it's a tie!");
+    return (textDiv.textContent = "it's a tie!");
   } else if (
     (playerSelection == rock && computerSelection == scissors) ||
     (playerSelection == paper && computerSelection == rock) ||
     (playerSelection == scissors && computerSelection == paper)
   ) {
     {
-      return console.log("you win");
+      pScore += 1;
+      playerScoreDiv.textContent = `Your's Score: ${pScore}`;
+      return (textDiv.textContent = "You win!");
     }
   } else {
-    return console.log("you lose");
+    vScore += 1;
+    vilainScoreDiv.textContent = `Vilain's Score: ${vScore}`;
+    return (textDiv.textContent = "You lose!");
   }
 }
 
@@ -49,25 +55,26 @@ function playGame(rounds) {
 // UI logic
 let container = document.querySelector("#container");
 
-function createElement(type, text, receptor) {
+function createElement(type, text, receptor, id = "") {
   let element = document.createElement(type);
   element.textContent = text;
   receptor.append(element);
+  element.setAttribute("id", id);
 }
 
 createElement("button", "rock", container);
 createElement("button", "paper", container);
 createElement("button", "scissors", container);
 createElement("div", "", container);
+createElement("div", `Your's Score: ${pScore}`, container, "pScore");
+createElement("div", `Vilain's Score: ${vScore}`, container, "vScore");
 
 let textDiv = container.querySelector("div");
+let vilainScoreDiv = container.querySelector("#vScore");
+let playerScoreDiv = container.querySelector("#pScore");
 
 let buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-  button.addEventListener(
-    "click",
-    (e) => (textDiv.textContent = e.target.textContent)
-  );
-});
 
-//playGame(roundsChoice);
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => playRound(e.target.textContent));
+});
